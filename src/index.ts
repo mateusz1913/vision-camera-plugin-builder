36c2e63a9356ed79774909bbd8080b62fa5cba9f
Mateusz Mędrek <mateusz.medrek@netguru.com>
2022-07-04 19:28:26 +0200

    chore: remove babel & add @vercel/ncc build setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
 import { androidArgs, androidCommandHandler } from './android';
 import { iosArgs, iosCommandHandler } from './ios';
 
-yargs
+yargs(hideBin(process.argv))
   .scriptName('vision-camera-plugin-builder')
   .usage('$0 <cmd> [args]')
   .command('ios', 'Generate iOS plugin boilerplate', iosArgs, iosCommandHandler)
