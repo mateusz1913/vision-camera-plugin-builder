69a1d56891ea57a8e54d5b7b11cc1dc18f9f3e36
Mateusz Mędrek <mateusz.medrek@callstack.com>
2024-01-09 00:27:32 +0100

    chore(#63): add integration tests for android & ios commands

---
+++ test/android.spec.ts
@@ -0,0 +1,107 @@
+import { vol } from 'memfs';
+
+import { androidCommandHandler } from '../src/android';
+
+import { expectFileContents } from './test-utils/expectFileContents';
+
+const MANIFEST_PACKAGE_NAME = 'com.myawesomeoldapp';
+const PROJECT_FILE_PATH = '/path/to/project';
+const ANDROID_APPLICATION_MANIFEST_FILE = `<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+        package="${MANIFEST_PACKAGE_NAME}">
+  <application>
+  </application>
+</manifest>
+`;
+
+jest.mock('fs');
+
+const manifestPath = `${PROJECT_FILE_PATH}/android/app/src/main/AndroidManifest.xml`;
+const pluginName = 'XyzFrameProcessor';
+const methodName = 'xyz';
+
+beforeEach(() => {
+  const JSONFileSystem = {
+    [`${PROJECT_FILE_PATH}/android/app/src/main/AndroidManifest.xml`]: ANDROID_APPLICATION_MANIFEST_FILE,
+    [`${PROJECT_FILE_PATH}/android/app/src/main/java/com/myawesomeoldapp/MainActivity.java`]: '',
+    [`${PROJECT_FILE_PATH}/android/app/src/main/java/com/myawesomeoldapp/MainApplication.java`]: '',
+  };
+
+  vol.fromJSON(JSONFileSystem);
+});
+
+afterEach(() => {
+  vol.reset();
+});
+
+describe('android', () => {
+  test('should create Kotlin plugin boilerplate', async () => {
+    await androidCommandHandler({
+      '$0': 'vision-camera-plugin-builder',
+      '_': [ 'android' ],
+      'manifestPath': manifestPath,
+      'manifest-path': manifestPath,
+      'pluginName': pluginName,
+      'plugin-name': pluginName,
+      'methodName': methodName,
+      'method-name': methodName,
+      'lang': 'Kotlin',
+    });
+
+    // XyzFrameProcessorPlugin.kt
+    const pluginFile = vol.readFileSync(`${PROJECT_FILE_PATH}/android/app/src/main/java/com/myawesomeoldapp/${pluginName.toLowerCase()}/${pluginName}Plugin.kt`, { encoding: 'utf-8' });
+
+    expectFileContents(pluginFile, [
+      'import com.mrousavy.camera.frameprocessor.Frame',
+      'import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin',
+      `class ${pluginName}Plugin(options: Map<String, Any>?): FrameProcessorPlugin(options)`,
+      'override fun callback(frame: Frame, arguments: Map<String, Any>?): Any?',
+    ]);
+
+    // XyzFrameProcessorPluginPackage.kt
+    const pluginPackageFile = vol.readFileSync(`${PROJECT_FILE_PATH}/android/app/src/main/java/com/myawesomeoldapp/${pluginName.toLowerCase()}/${pluginName}PluginPackage.kt`, { encoding: 'utf-8' });
+
+    expectFileContents(pluginPackageFile, [
+      'import com.facebook.react.ReactPackage',
+      'import com.mrousavy.camera.frameprocessor.FrameProcessorPluginRegistry',
+      `class ${pluginName}PluginPackage : ReactPackage`,
+      new RegExp(`FrameProcessorPluginRegistry.addFrameProcessorPlugin\\("${methodName}"\\) { options ->\\s+${pluginName}Plugin\\(options\\)\\s+}`),
+    ]);
+  });
+  test('should create Java plugin boilerplate', async () => {
+    await androidCommandHandler({
+      '$0': 'vision-camera-plugin-builder',
+      '_': [ 'android' ],
+      'manifestPath': manifestPath,
+      'manifest-path': manifestPath,
+      'pluginName': pluginName,
+      'plugin-name': pluginName,
+      'methodName': methodName,
+      'method-name': methodName,
+      'lang': 'java',
+    });
+
+    // XyzFrameProcessorPlugin.java
+    const pluginFile = vol.readFileSync(`${PROJECT_FILE_PATH}/android/app/src/main/java/com/myawesomeoldapp/${pluginName.toLowerCase()}/${pluginName}Plugin.java`, { encoding: 'utf-8' });
+
+    expectFileContents(pluginFile, [
+      'import androidx.annotation.NonNull;',
+      'import androidx.annotation.Nullable;',
+      'import java.util.Map;',
+      'import com.mrousavy.camera.frameprocessor.Frame;',
+      'import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;',
+      `public class ${pluginName}Plugin extends FrameProcessorPlugin`,
+      new RegExp(`public ${pluginName}Plugin\\(\\@Nullable Map<String, Object> options\\) {\\s+super\\(options\\);\\s+}`),
+      new RegExp('\\@Nullable\\s+\\@Override\\s+public Object callback\\(\\@NonNull Frame frame, \\@Nullable Map<String, Object> arguments\\)'),
+    ]);
+
+    // XyzFrameProcessorPluginPackage.java
+    const pluginPackageFile = vol.readFileSync(`${PROJECT_FILE_PATH}/android/app/src/main/java/com/myawesomeoldapp/${pluginName.toLowerCase()}/${pluginName}PluginPackage.java`, { encoding: 'utf-8' });
+
+    expectFileContents(pluginPackageFile, [
+      'import com.facebook.react.ReactPackage;',
+      'import com.mrousavy.camera.frameprocessor.FrameProcessorPluginRegistry;',
+      `public class ${pluginName}PluginPackage implements ReactPackage`,
+      new RegExp(`FrameProcessorPluginRegistry.addFrameProcessorPlugin\\(\\s+"${methodName}",\\s+options -> new ${pluginName}Plugin\\(options\\)\\s+\\);`),
+    ]);
+  });
+});
---
+++ test/ios.spec.ts
@@ -0,0 +1,112 @@
+import { vol } from 'memfs';
+
+import { iosCommandHandler } from '../src/ios';
+
+import { expectFileContents } from './test-utils/expectFileContents';
+
+const FIRST_TARGET_NAME = 'SamplePlugin';
+const PROJECT_FILE_PATH = '/path/to/project';
+const PODSPEC_FILE = `require "json"
+
+package = JSON.parse(File.read(File.join(__dir__, "package.json")))
+
+Pod::Spec.new do |s|
+  s.name         = "${FIRST_TARGET_NAME}"
+  s.version      = package["version"]
+  s.summary      = package["description"]
+  s.homepage     = package["homepage"]
+  s.license      = package["license"]
+  s.authors      = package["author"]
+
+  s.platforms    = { :ios => "12.4" }
+  s.source       = { :git => "https://path.to/repo.git", :tag => "#{s.version}" }
+
+  s.source_files = "ios/**/*.{h,m,mm,swift}"
+
+  s.dependency "React-Core"
+  s.dependency "VisionCamera"
+end
+`;
+
+jest.mock('fs');
+
+const projectPath = `${PROJECT_FILE_PATH}/ios`;
+const pluginName = 'XyzFrameProcessor';
+const methodName = 'xyz';
+
+beforeEach(() => {
+  const JSONFileSystem = {
+    [`${PROJECT_FILE_PATH}/ios`]: null,
+    [`${PROJECT_FILE_PATH}/SamplePlugin.podspec`]: PODSPEC_FILE,
+  };
+
+  vol.fromJSON(JSONFileSystem);
+});
+
+afterEach(() => {
+  vol.reset();
+});
+
+describe('ios', () => {
+  test('should create Swift plugin boilerplate', async () => {
+    await iosCommandHandler({
+      '$0': 'vision-camera-plugin-builder',
+      '_': [ 'ios' ],
+      'projectPath': projectPath,
+      'project-path': projectPath,
+      'pluginName': pluginName,
+      'plugin-name': pluginName,
+      'methodName': methodName,
+      'method-name': methodName,
+      'lang': 'Swift',
+    });
+
+    // XyzFrameProcessorPlugin.swift
+    const pluginFile = vol.readFileSync(`${PROJECT_FILE_PATH}/ios/${pluginName}/${pluginName}.swift`, { encoding: 'utf-8' });
+
+    expectFileContents(pluginFile, [
+      'import VisionCamera',
+      `@objc(${pluginName}Plugin)`,
+      `public class ${pluginName}Plugin: FrameProcessorPlugin`,
+      new RegExp('public override init\\(options: \\[AnyHashable: Any\\]\\! = \\[:\\]\\) {\\s+super.init\\(options: options\\)\\s+}'),
+      'public override func callback(_ frame: Frame, withArguments arguments: [AnyHashable: Any]?) -> Any?',
+    ]);
+
+    // XyzFrameProcessorPlugin.m
+    const pluginPackageFile = vol.readFileSync(`${PROJECT_FILE_PATH}/ios/${pluginName}/${pluginName}.m`, { encoding: 'utf-8' });
+
+    expectFileContents(pluginPackageFile, [
+      '#import <VisionCamera/FrameProcessorPlugin.h>',
+      '#import <VisionCamera/FrameProcessorPluginRegistry.h>',
+      new RegExp(`#if __has_include\\("${FIRST_TARGET_NAME}\\/${FIRST_TARGET_NAME}-Swift.h"\\)\\s+#import "${FIRST_TARGET_NAME}\\/${FIRST_TARGET_NAME}-Swift.h"\\s+#else\\s+#import "${FIRST_TARGET_NAME}-Swift.h"\\s+#endif`),
+      `VISION_EXPORT_SWIFT_FRAME_PROCESSOR(${pluginName}Plugin, ${methodName})`,
+    ]);
+  });
+  test.each([{ lang: 'ObjC' }, { lang: 'ObjCPP' }])('should create $lang plugin boilerplate', async ({ lang }) => {
+    await iosCommandHandler({
+      '$0': 'vision-camera-plugin-builder',
+      '_': [ 'ios' ],
+      'projectPath': projectPath,
+      'project-path': projectPath,
+      'pluginName': pluginName,
+      'plugin-name': pluginName,
+      'methodName': methodName,
+      'method-name': methodName,
+      'lang': lang,
+    });
+
+    // XyzFrameProcessorPlugin.(m|mm)
+    const pluginPackageFile = vol.readFileSync(`${PROJECT_FILE_PATH}/ios/${pluginName}/${pluginName}.${lang === 'ObjCPP' ? 'mm' : 'm'}`, { encoding: 'utf-8' });
+
+    expectFileContents(pluginPackageFile, [
+      '#import <VisionCamera/FrameProcessorPlugin.h>',
+      '#import <VisionCamera/FrameProcessorPluginRegistry.h>',
+      '#import <VisionCamera/Frame.h>',
+      `@interface ${pluginName}Plugin : FrameProcessorPlugin`,
+      `@implementation ${pluginName}Plugin`,
+      new RegExp('- \\(instancetype _Nonnull\\)initWithOptions:\\(NSDictionary\\* _Nullable\\)options\\s+{\\s+self = \\[super initWithOptions:options\\];\\s+return self;\\s+}'),
+      '- (id _Nullable)callback:(Frame* _Nonnull)frame withArguments:(NSDictionary* _Nullable)arguments',
+      `VISION_EXPORT_FRAME_PROCESSOR(${pluginName}Plugin, ${methodName})`,
+    ]);
+  });
+});
